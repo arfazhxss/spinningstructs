10a35c58ca7efc6ec220d8f6efa5f7e2cf66891e
Arfaz Hussain <arfazhussain@uvic.ca>
2023-05-09 00:26:10 -0700

    Routine Commit by @arfazhxss on Tue  9 May 2023 00:26:10 PDT

--- route.sh
+++ route.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# route git 3.8
 
 git add . && \
 git add -u && \
-git commit -m $'Routine Commit Session\n'"Committed on: [$(date)]"$'\nby Arfaz' && \
+git commit -m $'Routine Commit\nby @arfazhxss on '"$(date)" && \
 git push origin HEAD
