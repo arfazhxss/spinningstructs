814d43457a79fcd8b2cb5342e0f220a207beeb30
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-30 15:23:03 -0700

    Route Update Commit by @arfazhxss on Tue 30 May 2023

--- route.sh
+++ route.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# route git 3.8
+#auto-git v4.5
 
-git add . && \
-git add -u && \
-git commit -m $'Routine Commit\nby @arfazhxss on '"$(date)" && \
-git push origin HEAD
+echo "Commit Message:"
+read commit_message
+
+if [[ -z "$commit_message" || ${#commit_message} -lt 3 ]]; then
+  commit_message="Routine Commit"
+  git add . && \
+  git add -u && \
+  git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+  git push origin HEAD
+  git log > commit-hist.txt
+  rm -rf .DS_Store/
+else
+  git add . && \
+  git add -u && \
+  git commit -m $"$commit_message"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+  git push origin HEAD
+  git log > commit-hist.txt
+  rm -rf .DS_Store/
+fi
